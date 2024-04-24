c638d6863f1a720951c74864d95298c94528614c
chaudharytechy <amitchaud10@gmail.com>
2024-04-24 11:58:40 +0530

    copyrightissueresolved

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -12,16 +12,14 @@ const Footer = () => {
                 <p className="text-white" style={{ fontSize: '10px' }}><small>THIS SITE IS INFORMATION PURPOSE ONLY AND SHOULD NOT BE TREATED AS THE OFFICIAL INFORMATION</small></p>
 
                 <p className="text-white">
-                    <a href="https://www.delhishoppingcomplex.com" className='text-white'>Home</a> | 
-                    <a href="https://www.delhishoppingcomplex.com/contactus"className='text-white'>Contact</a> |
-                     <a href="https://www.delhishoppingcomplex.com/terms_condition"className='text-white'>Terms & Privacy</a> |
-                      <a href="https://www.delhishoppingcomplex.com/refund_policy"className='text-white'>Refunds & Cancellation</a> |
-                       <a href="https://www.delhishoppingcomplex.com/faq"className='text-white'>FAQ</a>
+                    <a href="" className='text-white'>Home</a> | 
+                    <a href=""className='text-white'>Contact</a> |
+                     <a href=""className='text-white'>Terms & Privacy</a> |
+                      <a href=""className='text-white'>Refunds & Cancellation</a> |
+                       <a href=""className='text-white'>FAQ</a>
                 </p>
 
-                <p className="text-white" style={{ fontSize: '10px' }}>
-                    <a href="https://www.stalwartsoft.com"className='text-white'>Developed by Stalwartsoft Technologies</a>
-                </p>
+               
             </div>
             <div className="col-md-2 text-white fw-bolder" id="count"></div>
         </div>
